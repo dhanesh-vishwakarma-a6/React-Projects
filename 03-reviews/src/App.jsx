93400e7453f580d75ce298840708f60f6d90ba32
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-01 21:21:52 +0530

    added heading & review component

---
+++ 03-reviews/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from "./components/Title/index"
+import Review from "./components/Review/index"
+
+function App() {
+  return <main>
+    <section className="container">
+      <Title />
+      <Review />
+    </section>
+  </main>;
+}
+
+export default App;
