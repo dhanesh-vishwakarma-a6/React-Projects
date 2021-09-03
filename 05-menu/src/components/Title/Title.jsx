27c6f426ab891bd5999ead03eaee0493dcdb2581
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-03 23:33:59 +0530

    added title components

---
+++ 05-menu/src/components/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./styles.css"
+
+const Title = () => {
+    return (
+        <header className="title">
+            <h1>our menu</h1>
+            <div className="underline"></div>
+        </header>
+    )
+}
+
+export default Title
