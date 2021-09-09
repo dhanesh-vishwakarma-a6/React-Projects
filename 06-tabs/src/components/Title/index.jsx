4e4b441f7032cec0d2a31c6342bd71ab3e0ee55a
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-09 20:39:47 +0530

    added Title

---
+++ 06-tabs/src/components/Title/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./style.css"
+
+const Title = () => {
+    return (
+        <div className="title">
+            <h1>experience</h1>
+            <div className="underline"></div>
+        </div>
+    )
+}
+
+export default Title
