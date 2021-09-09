03aa94543d38369d44ca4ccea1ab95cb5a4595e8
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-09 20:40:43 +0530

    added more info Button

---
+++ 06-tabs/src/components/MoreInfo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./style.css"
+
+const MoreInfo = () => {
+    return (
+        <button type="button" className="btn">
+            more info
+        </button>
+    )
+}
+
+export default MoreInfo
